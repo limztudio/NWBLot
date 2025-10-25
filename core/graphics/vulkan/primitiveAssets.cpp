0ed39b21118e9f5a297cbe0090bfe195b6723540
TeoLim <limztudio@gmail.com>
2025-10-26 01:17:42 +0900

    WIP compiler impl

--- core/graphics/vulkan/primitiveAssets.cpp
+++ core/graphics/vulkan/primitiveAssets.cpp
@@ -64,10 +64,329 @@ bool SpirVParseResult::parse(const void* raw, usize size, Alloc::CustomArena& ar
     NWB_ASSERT(data[0] == 0x07230203);
 
     const u32 idBound = data[3];
-
     CustomUniquePtr<SpirVId[]> ids = makeCustomUnique<SpirVId[]>(arena, idBound);
     
-    VkShaderStageFlags stage;
+    VkShaderStageFlags stage = 0;
+
+    usize wordIndex = 5;
+    while(wordIndex < numWord){
+        SpvOp op = static_cast<decltype(op)>(data[wordIndex] & 0xff);
+        u16 wordCount = static_cast<decltype(wordCount)>(data[wordIndex] >> 16);
+
+        switch(op){
+        case SpvOpEntryPoint:
+        {
+            NWB_ASSERT(wordCount >= 4);
+
+            SpvExecutionModel model = static_cast<decltype(model)>(data[wordIndex + 1]);
+            switch(model){
+            case SpvExecutionModelVertex:
+                stage = VK_SHADER_STAGE_VERTEX_BIT;
+                break;
+            case SpvExecutionModelGeometry:
+                stage = VK_SHADER_STAGE_GEOMETRY_BIT;
+                break;
+            case SpvExecutionModelFragment:
+                stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+                break;
+            case SpvExecutionModelKernel:
+                stage = VK_SHADER_STAGE_COMPUTE_BIT;
+                break;
+            }
+            NWB_ASSERT(stage != 0);
+        }
+        break;
+
+        case SpvOpDecorate:
+        {
+            NWB_ASSERT(wordCount >= 3);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+
+            SpvDecoration decoration = static_cast<decltype(decoration)>(data[wordIndex + 2]);
+            switch(decoration){
+            case SpvDecorationBinding:
+                id.binding = data[wordIndex + 3];
+                break;
+            case SpvDecorationDescriptorSet:
+                id.set = data[wordIndex + 3];
+                break;
+            }
+        }
+        break;
+
+        case SpvOpMemberDecorate:
+        {
+            NWB_ASSERT(wordCount >= 4);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+
+            u32 memberIndex = data[wordIndex + 2];
+
+            if(!id.members)
+                id.members = makeCustomUnique<SpirVMember[]>(arena, 64);
+
+            SpirVMember& member = id.members[memberIndex];
+
+            SpvDecoration decoration = static_cast<decltype(decoration)>(data[wordIndex + 3]);
+            switch(decoration){
+            case SpvDecorationOffset:
+                member.offset = data[wordIndex + 4];
+                break;
+            }
+        }
+        break;
+
+        case SpvOpName:
+        {
+            NWB_ASSERT(wordCount >= 3);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+
+            char* name = nullptr;
+            {
+                const auto* raw = reinterpret_cast<const char*>(data + (wordIndex + 2));
+                const usize len = NWB_STRLEN(raw);
+
+                name = arena.allocate<char>(len + 1);
+                NWB_STRNCPY(name, len, raw, len);
+                name[len] = 0;
+            }
+            id.name = name;
+        }
+        break;
+
+        case SpvOpMemberName:
+        {
+            NWB_ASSERT(wordCount >= 4);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+
+            u32 memberIndex = data[wordIndex + 2];
+
+            if(!id.members)
+                id.members = makeCustomUnique<SpirVMember[]>(arena, 64);
+
+            SpirVMember& member = id.members[memberIndex];
+
+            char* name = nullptr;
+            {
+                const auto* raw = reinterpret_cast<const char*>(data + (wordIndex + 3));
+                const usize len = NWB_STRLEN(raw);
+                name = arena.allocate<char>(len + 1);
+                NWB_STRNCPY(name, len, raw, len);
+                name[len] = 0;
+            }
+
+            member.name = name;
+        }
+        break;
+
+        case SpvOpTypeInt:
+        {
+            NWB_ASSERT(wordCount == 4);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+            id.width = static_cast<u8>(data[wordIndex + 2]);
+            id.sign = static_cast<u8>(data[wordIndex + 3]);
+        }
+        break;
+
+        case SpvOpTypeFloat:
+        {
+            NWB_ASSERT(wordCount == 3);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+            id.width = static_cast<u8>(data[wordIndex + 2]);
+        }
+        break;
+
+        case SpvOpTypeVector:
+        {
+            NWB_ASSERT(wordCount == 4);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+            id.typeIndex = data[wordIndex + 2];
+            id.count = data[wordIndex + 3];
+        }
+        break;
+
+        case SpvOpTypeMatrix:
+        {
+            NWB_ASSERT(wordCount == 4);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+            id.typeIndex = data[wordIndex + 2];
+            id.count = data[wordIndex + 3];
+        }
+        break;
+
+        case SpvOpTypeImage:
+        {
+            NWB_ASSERT(wordCount >= 9);
+            // not implemented yet
+        }
+        break;
+
+        case SpvOpTypeSampler:
+        {
+            NWB_ASSERT(wordCount == 2);
+            
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+        }
+        break;
+
+        case SpvOpTypeSampledImage:
+        {
+            NWB_ASSERT(wordCount == 3);
+            
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+        }
+        break;
+
+        case SpvOpTypeArray:
+        {
+            NWB_ASSERT(wordCount == 4);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+            id.typeIndex = data[wordIndex + 2];
+            id.count = data[wordIndex + 3];
+        }
+        break;
+
+        case SpvOpTypeRuntimeArray:
+        {
+            NWB_ASSERT(wordCount == 3);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+            id.typeIndex = data[wordIndex + 2];
+        }
+        break;
+
+        case SpvOpTypeStruct:
+        {
+            NWB_ASSERT(wordCount >= 2);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+
+            if(wordCount > 2){
+                for(u16 memberIndex = 0; memberIndex < wordCount - 2; ++memberIndex)
+                    id.members[memberIndex].idIndex = data[wordIndex + memberIndex + 2];
+            }
+        }
+        break;
+
+        case SpvOpTypePointer:
+        {
+            NWB_ASSERT(wordCount == 4);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+            id.typeIndex = data[wordIndex + 3];
+        }
+        break;
+
+        case SpvOpConstant:
+        {
+            NWB_ASSERT(wordCount >= 4);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+            id.typeIndex = data[wordIndex + 2];
+            id.value = data[wordIndex + 3]; // assume all constants are 32-bit for now
+        }
+        break;
+
+        case SpvOpVariable:
+        {
+            NWB_ASSERT(wordCount == 4);
+
+            u32 idIndex = data[wordIndex + 1];
+            NWB_ASSERT(idIndex < idBound);
+
+            SpirVId& id = ids[idIndex];
+            id.op = op;
+            id.typeIndex = data[wordIndex + 1];
+            id.storageClass = static_cast<decltype(id.storageClass)>(data[wordIndex + 3]);
+        }
+        break;
+        }
+
+        wordIndex += wordCount;
+    }
+
+    for(u32 idIndex = 0; idIndex < idBound; ++idIndex){
+        SpirVId& id = ids[idIndex];
+
+        if(id.op == SpvOpVariable){
+            switch(id.storageClass){
+            case SpvStorageClassUniform:
+            case SpvStorageClassUniformConstant:
+            {
+                if(
+                (id.set == 1)
+                
+                )
+                continue;
+            }
+            break;
+            }
+        }
+    }
 }
 
 

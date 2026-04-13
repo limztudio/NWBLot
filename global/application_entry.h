7adef2ce6ed14a860678f270e9b412fcbe433e31
Teo Lim <limztudio@gmail.com>
2026-04-14 07:47:54 +0900

    Fix Windows Unicode entry argv handling

--- global/application_entry.h
+++ global/application_entry.h
@@ -7,8 +7,6 @@
 
 #include "platform.h"
 #include "type.h"
-#include "basic_string.h"
-#include "containers.h"
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -19,43 +17,69 @@
 #include <windows.h>
 #if defined(NWB_UNICODE)
 #include <shellapi.h>
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+namespace ApplicationEntryDetail{
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class WindowsCommandLineArgs final{
+public:
+    WindowsCommandLineArgs(){
+        m_argv = CommandLineToArgvW(GetCommandLineW(), &m_argc);
+    }
+    ~WindowsCommandLineArgs(){
+        if(m_argv)
+            LocalFree(m_argv);
+    }
+
+    WindowsCommandLineArgs(const WindowsCommandLineArgs&) = delete;
+    WindowsCommandLineArgs& operator=(const WindowsCommandLineArgs&) = delete;
+
+    [[nodiscard]] bool valid()const{ return m_argv != nullptr; }
+    [[nodiscard]] isize argc()const{ return static_cast<isize>(m_argc); }
+    [[nodiscard]] wchar** argv()const{ return m_argv; }
+
+private:
+    int m_argc = 0;
+    LPWSTR* m_argv = nullptr;
+};
+
+template<typename EntryPoint>
+[[nodiscard]] inline int InvokeWindowsUnicodeEntryPoint(EntryPoint entryPoint, HINSTANCE hInstance){
+    WindowsCommandLineArgs args;
+    if(args.valid())
+        return entryPoint(args.argc(), args.argv(), hInstance);
+
+    wchar emptyProgramName[] = L"";
+    wchar* emptyArgv[] = { emptyProgramName };
+    return entryPoint(static_cast<isize>(1), emptyArgv, hInstance);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+};
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 #define NWB_DEFINE_APPLICATION_ENTRY_POINT(entryPoint) \
     int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow){ \
         (void)hPrevInstance; \
         (void)lpCmdLine; \
         (void)nCmdShow; \
-        int nwbArgc = 0; \
-        LPWSTR* nwbArgv = CommandLineToArgvW(GetCommandLineW(), &nwbArgc); \
-        if(nwbArgv){ \
-            const int nwbReturnCode = entryPoint(static_cast<isize>(nwbArgc), nwbArgv, hInstance); \
-            LocalFree(nwbArgv); \
-            return nwbReturnCode; \
-        } \
-        wchar nwbEmptyProgramName[] = L""; \
-        wchar* nwbEmptyArgv[] = { nwbEmptyProgramName }; \
-        return entryPoint(static_cast<isize>(1), nwbEmptyArgv, hInstance); \
+        return ApplicationEntryDetail::InvokeWindowsUnicodeEntryPoint(entryPoint, hInstance); \
     } \
-    int main(int argc, char** argv){ \
-        const usize nwbArgCount = argc > 0 ? static_cast<usize>(argc) : 0; \
-        Vector<WString> nwbWideArgs; \
-        Vector<wchar*> nwbWideArgv; \
-        nwbWideArgs.reserve(nwbArgCount > 0 ? nwbArgCount : 1); \
-        nwbWideArgv.reserve(nwbArgCount > 0 ? nwbArgCount : 1); \
-        if(nwbArgCount == 0 || argv == nullptr){ \
-            nwbWideArgs.push_back(WString()); \
-            nwbWideArgv.push_back(nwbWideArgs.back().data()); \
-        } \
-        else{ \
-            for(usize nwbArgIndex = 0; nwbArgIndex < nwbArgCount; ++nwbArgIndex){ \
-                if(argv[nwbArgIndex] == nullptr){ \
-                    nwbWideArgv.push_back(nullptr); \
-                    continue; \
-                } \
-                nwbWideArgs.push_back(StringConvert(AStringView(argv[nwbArgIndex]))); \
-                nwbWideArgv.push_back(nwbWideArgs.back().data()); \
-            } \
-        } \
-        return entryPoint(static_cast<isize>(nwbWideArgv.size()), nwbWideArgv.data(), GetModuleHandleW(nullptr)); \
+    int main(int, char**){ \
+        return ApplicationEntryDetail::InvokeWindowsUnicodeEntryPoint(entryPoint, GetModuleHandleW(nullptr)); \
     }
 #else
 #define NWB_DEFINE_APPLICATION_ENTRY_POINT(entryPoint) \

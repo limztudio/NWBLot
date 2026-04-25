495bf61b05bfe03d349827c2694a7f19f27dd28c
Teo Lim <limztudio@gmail.com>
2026-04-25 16:34:23 +0900

    Optimize mesh shader material colors

--- impl/assets/graphics/bxdf_ms.glsl
+++ impl/assets/graphics/bxdf_ms.glsl
@@ -6,6 +6,7 @@
 #include "mesh_shader_authoring.glsli"
 #include "bxdf.glsli"
 
+const uvec2 NWB_ENGINE_MATERIAL_BASE_COLOR = uvec2(0x75bad3d8u, 0x5c10c7e2u);
 const uvec2 NWB_ENGINE_MATERIAL_COLOR_TINT = uvec2(0x360abc2cu, 0xca7492edu);
 
 vec4 nwbEngineBuildClipPosition(vec3 worldPosition){
@@ -14,7 +15,6 @@ vec4 nwbEngineBuildClipPosition(vec3 worldPosition){
 
 NWB_MESH_BUILD_VERTEX_SIGNATURE{
     NwbMeshGeneratedVertex generatedVertex;
-    const NwbMeshInstanceData instance = nwbMeshLoadInstance();
     const vec3 worldPosition = nwbMeshTransformPosition(source.position, instance);
     generatedVertex.position = nwbEngineBuildClipPosition(worldPosition);
     generatedVertex.normal = nwbMeshTransformDirection(source.normal, instance);
@@ -22,7 +22,8 @@ NWB_MESH_BUILD_VERTEX_SIGNATURE{
     generatedVertex.tangent = vec4(nwbMeshTransformDirection(source.tangent.xyz, instance), source.tangent.w);
     generatedVertex.uv0 = source.uv0;
     generatedVertex.padding1 = vec2(0.0);
-    const vec4 tintedColor = source.color * nwbMaterialFindFloat4(instance, NWB_ENGINE_MATERIAL_COLOR_TINT, vec4(1.0));
+    const vec4 materialColor = nwbMaterialFindFloat4(instance, NWB_ENGINE_MATERIAL_BASE_COLOR, source.color);
+    const vec4 tintedColor = materialColor * nwbMaterialFindFloat4(instance, NWB_ENGINE_MATERIAL_COLOR_TINT, vec4(1.0));
     generatedVertex.color = vec4(
         clamp(nwbProjectBxdfVertex(tintedColor.rgb), vec3(0.0), vec3(1.0)),
         tintedColor.a

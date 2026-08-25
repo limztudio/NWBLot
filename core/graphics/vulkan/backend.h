672aca91aefcd4b2533ed159de807ed6191965b0
TeoLim <limztudio@gmail.com>
2026-08-25 13:19:10 +0900

    Refactor texture and buffer state declarations
    Reorganized the declaration of m_attemptPermanentTextureStates and m_attemptPermanentBufferStates for consistency.

--- core/graphics/vulkan/backend.h
+++ core/graphics/vulkan/backend.h
@@ -2245,12 +2245,8 @@ private:
 private:
     HashMap<Texture*, ResourceStates::Mask, Hasher<Texture*>, EqualTo<Texture*>, Alloc::GlobalArena> m_permanentTextureStates;
     HashMap<Buffer*, ResourceStates::Mask, Hasher<Buffer*>, EqualTo<Buffer*>, Alloc::GlobalArena> m_permanentBufferStates;
-    HashMap<
-        Texture*, ResourceStates::Mask, Hasher<Texture*>, EqualTo<Texture*>, Alloc::GlobalArena
-    > m_attemptPermanentTextureStates;
-    HashMap<
-        Buffer*, ResourceStates::Mask, Hasher<Buffer*>, EqualTo<Buffer*>, Alloc::GlobalArena
-    > m_attemptPermanentBufferStates;
+    HashMap<Texture*, ResourceStates::Mask, Hasher<Texture*>, EqualTo<Texture*>, Alloc::GlobalArena> m_attemptPermanentTextureStates;
+    HashMap<Buffer*, ResourceStates::Mask, Hasher<Buffer*>, EqualTo<Buffer*>, Alloc::GlobalArena> m_attemptPermanentBufferStates;
     HashMap<TextureSubresourceStateKey, ResourceStates::Mask, TextureSubresourceStateKeyHasher, TextureSubresourceStateKeyEqualTo, Alloc::GlobalArena> m_textureStates;
     HashMap<Buffer*, ResourceStates::Mask, Hasher<Buffer*>, EqualTo<Buffer*>, Alloc::GlobalArena> m_bufferStates;
     HashMap<Texture*, bool, Hasher<Texture*>, EqualTo<Texture*>, Alloc::GlobalArena> m_textureUavBarriers;

cdd0b09f33b67752ae0e295e197f421a7d16e943
TeoLim <limztudio@gmail.com>
2026-09-15 14:34:59 +0900

    Merge 37 tripled AVBOIT record input fields into shared base struct

--- impl/ecs_render/avboit/accumulation_record_builder.h
+++ impl/ecs_render/avboit/accumulation_record_builder.h
@@ -12,6 +12,7 @@
 #include <impl/ecs_render/csg/csg_graph_resource_snapshot.h>
 #include <impl/ecs_render/csg/task_graph_opaque_compute_emulation_plan.h>
 #include <impl/ecs_render/avboit/task_graph_compute_emulation_plan.h>
+#include <impl/ecs_render/avboit/record_inputs_base.h>
 #include <impl/ecs_render/avboit/task_graph_accumulation_tasks.h>
 
 #include <core/graphics/gpu_timing.h>
@@ -36,13 +37,7 @@ struct DeferredFrameTargets;
 
 
 // AVBOIT accumulation record owns compute-emulation plus shared-phase plus raster declaration.
-struct AvboitAccumulationRecordInputs{
-    DeferredFrameTargets* targets = nullptr;
-    Core::GpuGraphResourceId albedo;
-    Core::GpuGraphResourceId normal;
-    Core::GpuGraphResourceId worldPosition;
-    Core::GpuGraphResourceId depth;
-    Core::GpuGraphResourceId refractionInstance;
+struct AvboitAccumulationRecordInputs : public AvboitRecordInputsBase{
     Core::GpuGraphResourceId refractionDepth;
     Core::GpuGraphResourceId avboitDepthWarp;
     Core::GpuGraphResourceId avboitTransmittance;
@@ -51,46 +46,16 @@ struct AvboitAccumulationRecordInputs{
     Core::GpuGraphResourceId avboitForegroundExtinction;
     Core::GpuGraphResourceId avboitAccumColor;
     Core::GpuGraphResourceId avboitAccumExtinction;
-    Core::GpuGraphResourceId avboitMaterialDomain;
-    Core::GpuGraphResourceId avboitCsgDomain;
-    Core::GpuGraphResourceId meshView;
-    Core::GpuGraphResourceId materialInstances;
-    Core::GpuGraphResourceId materialTyped;
-    Core::GpuGraphResourceId csgReceiverRanges;
-    Core::GpuGraphResourceId csgCutters;
-    Core::GpuGraphResourceId csgClipContextSlots;
-    Core::GpuGraphResourceId csgIntervalSampleState;
-    Core::GpuGraphResourceId csgRemovedIntervalDepth;
-    Core::GpuGraphResourceId csgRemovedIntervalCapNormal;
-    Core::GpuGraphResourceId csgRemovedIntervalData;
-    Core::GpuGraphResourceId csgRemovedIntervalCount;
-    Core::TextureSubresourceSet csgRemovedIntervalSubresources;
-    Core::TextureSubresourceSet csgRemovedIntervalCountSubresources;
-    Core::GpuGraphResourceId currentBindlessSlots;
     Core::GpuTaskId integrationTask;
-    Core::GpuTaskId uploadTask;
-    Core::GpuGraphResourceSetId materialGeometrySet;
-    Core::GpuGraphResourceSetId materialSampledTextureSet;
-    Core::BufferRange instanceRange;
-    Core::BufferRange materialTypedRange;
-    Core::BufferRange receiverRange;
-    Core::BufferRange cutterRange;
-    bool intervalOutputsGraphOwned = false;
-    bool csgStreamsUploaded = false;
     bool streamsUploaded = false;
-    bool regularComputeEmulationPlanCaptured = false;
-    bool csgComputeEmulationPlanCaptured = false;
-    bool sharedComputeEmulationPlanCaptured = false;
-    ECSRenderDetail::RegularSharedComputeEmulationGraphPlan sharedComputeEmulationPlan;
-    usize sharedComputeEmulationInstanceCount = 0u;
-    usize sharedComputeEmulationMaterialTypedByteCount = 0u;
     Core::GpuTimingSubmissionTicket* accumulationTimingTicket = nullptr;
     Optional<Core::GpuTimingMeasure>* accumulationComputeEmulationTiming = nullptr;
 
-    explicit AvboitAccumulationRecordInputs(Core::Alloc::GlobalArena& arena){
-        static_cast<void>(arena);
+    explicit AvboitAccumulationRecordInputs(Core::Alloc::GlobalArena& arena)
+        : AvboitRecordInputsBase(arena){
     }
 };
+
 
 struct AvboitAccumulationRecordResult{
     Core::GpuTaskId accumulationTask;
